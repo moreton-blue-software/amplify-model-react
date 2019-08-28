fd8c0b732c4e1f9f43a39efecb4470ed8b9c5738
jim <thecastlefaller@gmail.com>
2019-08-28 13:43:18 +0800

    threads ui transpile

--- dist/index.js
+++ dist/index.js
@@ -1,136 +1,141 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.ModelFieldTextSelector = exports.ModelSelector = exports.ModelFieldSelector = exports.ModelFieldInput = exports.ModelFieldFile = exports.ModelFieldDate = exports.ModelFieldDateTime = exports.ModelFieldControl = exports.AmplifyModelProvider = exports.AmplifyModelContext = exports.useModelForm = exports.ModelFormContext = exports.ModelForm = exports.TestButton = exports.Alerts = undefined;
+exports.ModelFieldTextSelector = exports.ModelSelector = exports.ModelFieldSelector = exports.ModelFieldInput = exports.ModelFieldFile = exports.ModelFieldDate = exports.ModelFieldDateTime = exports.ModelFieldControl = exports.AmplifyModelProvider = exports.AmplifyModelContext = exports.useModelForm = exports.ModelFormContext = exports.ModelForm = exports.TestButton = exports.Thread = exports.Alerts = undefined;
 
-var _Button = require("./Button");
+var _Button = require('./Button');
 
-Object.defineProperty(exports, "TestButton", {
+Object.defineProperty(exports, 'TestButton', {
   enumerable: true,
   get: function get() {
     return _interopRequireDefault(_Button).default;
   }
 });
 
-var _ModelForm = require("./ModelForm");
+var _ModelForm = require('./ModelForm');
 
-Object.defineProperty(exports, "ModelForm", {
+Object.defineProperty(exports, 'ModelForm', {
   enumerable: true,
   get: function get() {
     return _interopRequireDefault(_ModelForm).default;
   }
 });
-Object.defineProperty(exports, "ModelFormContext", {
+Object.defineProperty(exports, 'ModelFormContext', {
   enumerable: true,
   get: function get() {
     return _ModelForm.ModelFormContext;
   }
 });
-Object.defineProperty(exports, "useModelForm", {
+Object.defineProperty(exports, 'useModelForm', {
   enumerable: true,
   get: function get() {
     return _ModelForm.useModelForm;
   }
 });
 
-var _ModelFormController = require("./ModelFormController");
+var _ModelFormController = require('./ModelFormController');
 
-Object.defineProperty(exports, "AmplifyModelContext", {
+Object.defineProperty(exports, 'AmplifyModelContext', {
   enumerable: true,
   get: function get() {
     return _interopRequireDefault(_ModelFormController).default;
   }
 });
-Object.defineProperty(exports, "AmplifyModelProvider", {
+Object.defineProperty(exports, 'AmplifyModelProvider', {
   enumerable: true,
   get: function get() {
     return _ModelFormController.ModelFormControllerProvider;
   }
 });
 
-var _ModelControl = require("./ModelControl");
+var _ModelControl = require('./ModelControl');
 
-Object.defineProperty(exports, "ModelFieldControl", {
+Object.defineProperty(exports, 'ModelFieldControl', {
   enumerable: true,
   get: function get() {
     return _interopRequireDefault(_ModelControl).default;
   }
 });
 
-var _ModelFieldDateTime = require("./ModelFieldDateTime");
+var _ModelFieldDateTime = require('./ModelFieldDateTime');
 
-Object.defineProperty(exports, "ModelFieldDateTime", {
+Object.defineProperty(exports, 'ModelFieldDateTime', {
   enumerable: true,
   get: function get() {
     return _interopRequireDefault(_ModelFieldDateTime).default;
   }
 });
 
-var _ModelFieldDate = require("./ModelFieldDate");
+var _ModelFieldDate = require('./ModelFieldDate');
 
-Object.defineProperty(exports, "ModelFieldDate", {
+Object.defineProperty(exports, 'ModelFieldDate', {
   enumerable: true,
   get: function get() {
     return _interopRequireDefault(_ModelFieldDate).default;
   }
 });
 
-var _ModelFieldFile = require("./ModelFieldFile");
+var _ModelFieldFile = require('./ModelFieldFile');
 
-Object.defineProperty(exports, "ModelFieldFile", {
+Object.defineProperty(exports, 'ModelFieldFile', {
   enumerable: true,
   get: function get() {
     return _interopRequireDefault(_ModelFieldFile).default;
   }
 });
 
-var _ModelFieldInput = require("./ModelFieldInput");
+var _ModelFieldInput = require('./ModelFieldInput');
 
-Object.defineProperty(exports, "ModelFieldInput", {
+Object.defineProperty(exports, 'ModelFieldInput', {
   enumerable: true,
   get: function get() {
     return _interopRequireDefault(_ModelFieldInput).default;
   }
 });
 
-var _ModelFieldSelector = require("./ModelFieldSelector");
+var _ModelFieldSelector = require('./ModelFieldSelector');
 
-Object.defineProperty(exports, "ModelFieldSelector", {
+Object.defineProperty(exports, 'ModelFieldSelector', {
   enumerable: true,
   get: function get() {
     return _interopRequireDefault(_ModelFieldSelector).default;
   }
 });
 
-var _ModelSelector = require("./ModelSelector");
+var _ModelSelector = require('./ModelSelector');
 
-Object.defineProperty(exports, "ModelSelector", {
+Object.defineProperty(exports, 'ModelSelector', {
   enumerable: true,
   get: function get() {
     return _interopRequireDefault(_ModelSelector).default;
   }
 });
 
-var _ModelFieldTextSelector = require("./ModelFieldTextSelector");
+var _ModelFieldTextSelector = require('./ModelFieldTextSelector');
 
-Object.defineProperty(exports, "ModelFieldTextSelector", {
+Object.defineProperty(exports, 'ModelFieldTextSelector', {
   enumerable: true,
   get: function get() {
     return _interopRequireDefault(_ModelFieldTextSelector).default;
   }
 });
 
-var _Alerts = require("./Utils/Alerts");
+var _Alerts = require('./Utils/Alerts');
 
 var Alerts = _interopRequireWildcard(_Alerts);
+
+var _Thread = require('./Utils/Thread');
+
+var _Thread2 = _interopRequireDefault(_Thread);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+//utils
 exports.Alerts = Alerts;
+exports.Thread = _Thread2.default;
 
 //modules
-//utils
