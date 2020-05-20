e8937b46751964c768715a9b6c1ec8123d28b4e0
jim <thecastlefaller@gmail.com>
2020-05-20 10:00:56 +0800

    fix required tag for modelfielddatetime

--- src/modules/ModelFieldDateTime/index.js
+++ src/modules/ModelFieldDateTime/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { ModelFormContext, useModelForm } from "../ModelForm";
-import startCase from "lodash/startCase";
-import FormControl from "@material-ui/core/FormControl";
-import padStart from "lodash/padStart";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import { DateTimePicker, DatePicker } from "@material-ui/pickers";
-import RequiredTag, { requiredTagText } from "../common/RequiredTag";
+import React from 'react';
+import { useModelForm } from '../ModelForm';
+import startCase from 'lodash/startCase';
+import FormControl from '@material-ui/core/FormControl';
+import padStart from 'lodash/padStart';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import { DateTimePicker, DatePicker } from '@material-ui/pickers';
+import RequiredTag from '../common/RequiredTag';
 
 export default function ModelFieldDateTime(props) {
   const {
@@ -13,7 +13,7 @@ export default function ModelFieldDateTime(props) {
     dateOnly: dateOnlyTmp = false,
     label,
     strictDate = false,
-    pickerProps = {}
+    pickerProps = {},
   } = props;
   const labelText = label || startCase(field);
   const { form, control } = useModelForm({ field });
@@ -21,24 +21,25 @@ export default function ModelFieldDateTime(props) {
   const rawValue = handlers.getFieldValue(field);
   const dateOnly = strictDate || dateOnlyTmp;
   const Picker = dateOnly ? DatePicker : DateTimePicker;
-  const value = rawValue ? new Date(rawValue) : "";
+  const value = rawValue ? new Date(rawValue) : '';
   const checkDate = React.useCallback(
     mDate => {
       control && control.setTouched(true);
       // if (!mDate || !mDate._d) return;
       const minuteOffset = mDate.getTimezoneOffset();
-      let date = new Date(mDate.getTime());
+      const date = new Date(mDate.getTime());
       date.setMinutes(date.getMinutes() + minuteOffset);
-      let month = padStart(date.getMonth() + 1, 2, "0");
-      let day = padStart(date.getDate(), 2, "0");
+      const month = padStart(date.getMonth() + 1, 2, '0');
+      const day = padStart(date.getDate(), 2, '0');
       const year = date.getFullYear();
-      const hours = padStart(date.getHours(), 2, "0");
-      const minutes = padStart(date.getMinutes(), 2, "0");
+      const hours = padStart(date.getHours(), 2, '0');
+      const minutes = padStart(date.getMinutes(), 2, '0');
       let awsDate = `${year}-${month}-${day}`;
       const awsDateTime = awsDate + `T${hours}:${minutes}:00.000Z`;
-      if (strictDate === false) awsDate = awsDate + `T00:00:00.000Z`;
+      if (strictDate === false) awsDate = awsDate + 'T00:00:00.000Z';
       handlers.setFieldValue(field, dateOnly ? awsDate : awsDateTime);
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [field, handlers]
   );
   return (
@@ -46,13 +47,13 @@ export default function ModelFieldDateTime(props) {
       <FormControl fullWidth>
         <Picker
           {...pickerProps}
-          value={value === "" ? null : value}
+          value={value === '' ? null : value}
           onChange={checkDate}
-          label={labelText + (control.required ? requiredTagText() : "")}
+          label={labelText + (control.required ? <RequiredTag /> : '')}
           error={control.hasError}
         />
         {control.hasErrors && (
-          <FormHelperText style={{ color: "red" }}>
+          <FormHelperText style={{ color: 'red' }}>
             {control.errors.map(error => {
               return error;
             })}
